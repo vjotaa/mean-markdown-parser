7e918d7931f8ab221e7d84c08b5e03a011558a46
vjotaa <vjotaa@gmail.com>
2018-04-17 22:42:43 -0400

    changes

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -1,33 +1,32 @@
 const Document = require('../models/document');
 
 module.exports = {
-  documents: async (req, res, next) => {
+  documents: async (req, res) => {
     const documents = await Document.find({});
     res.status(200).json(documents);
   },
-  newDocument: async (req, res, next) => {
+  newDocument: async (req, res) => {
     document = new Document();
     document.title = req.body.title;
     document.text = req.body.text;
-    //sconst newDocument = new Document(document);
     const documentStorage = await document.save();
     res.status(200).json(documentStorage);
   },
 
-  document: async (req, res, next) => {
+  document: async (req, res) => {
     const { id } = req.params;
     const document = await Document.findById(id);
     res.status(200).json(document);
   },
 
-  updateDocument: async (req, res, next) => {
+  updateDocument: async (req, res) => {
     const { id } = req.params;
     const newDocument = req.body;
     const oldDocument = await Document.findByIdAndUpdate(id, newDocument);
     res.status(200).json({ success: true });
   },
 
-  deleteDocument: async (req, res, next) => {
+  deleteDocument: async (req, res) => {
     const { id } = req.params;
     const newDocument = req.body;
     const oldDocument = await Document.findByIdAndRemove(id, newDocument);
