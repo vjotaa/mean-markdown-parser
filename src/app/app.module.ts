8a92440867a281ba3fa9e69800270aba79dcc9c9
vjotaa <vjotaa@gmail.com>
2018-04-22 22:35:44 -0400

    breaking changes of http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import { UpdateDocumentComponent } from './document/update-document/update-docum
     TimeAgoPipe,
     UpdateDocumentComponent
   ],
-  imports: [BrowserModule, HttpModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
